e3d76a4f512aad0d9c50d35ea117d1a4bef8f3c8
Jan Blaesi <jan@blaesi.com>
2021-05-05 14:54:35 +0200

    enable CORS middleware

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
+
+	router.Use(cors.Default())
 
 	initDb()
 	setupRoutes(router)
